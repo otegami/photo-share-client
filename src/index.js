820c07e14b4bcd97b5f95bedbdb8df3ccae00a13
otegami <a.s.takuya1026@gmail.com>
2021-09-06 19:20:14 -0700

    6.3.3 Query コンポーネント

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 import { ApolloProvider } from 'react-apollo'
-import ApolloClient, { gql } from 'apollo-boost'
+import ApolloClient from 'apollo-boost'
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
@@ -12,61 +12,3 @@ render(
   </ApolloProvider>,
   document.getElementById('root')
 )
-
-// const query = gql`
-//   {
-//     totalUsers
-//     totalPhotos
-//   }
-// `
-
-// client.query({query})
-//   .then(({ data }) => console.log('data', data))
-//   .then(console.error)
-
-// console.log('cache', client.extract())
-// client.query({query})
-//   .then(() => console.log('cache', client.extract()))
-//   .catch(console.error)
-
-// let mutation = `
-//   mutation populate($count: Int!) {
-//     addFakeUsers(count: $count) {
-//       githubLogin
-//       avatar
-//       name
-//     }
-//   }
-// `
-
-// const App = ({ users = [] }) => {
-//   return(
-//     <div>
-//       {users.map(user =>
-//         <div key={user.githubLogin}>
-//           <img src={user.avatar} alt="" />
-//           {user.name}
-//         </div>
-//       )}
-//       <button  onClick={addUser}>Add User</button>
-//     </div>
-//   )
-// }
-
-// const render = ({ allUsers = [] }) =>
-//   ReactDOM.render(
-//       <App users={allUsers} />,
-//     document.getElementById('root')
-//   )
-
-// const addUser = () =>
-//   request(url, mutation, {count: 1})
-//     .then(requestAndRendar)
-//     .catch(console.error)
-
-// const requestAndRendar = () =>
-//   request(url, query)
-//     .then(render)
-//     .catch(console.error)
-
-// requestAndRendar()
